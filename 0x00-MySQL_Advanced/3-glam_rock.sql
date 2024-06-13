6061a695cd91ecf8875e67bd0a2d85b440833384
kageruka02 <leonkageruka@gmail.com>
2024-06-14 00:31:19 +0300

    calculates the life span

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- Column names must be: band_name and lifespan (in years until 2022 - please use 2022 instead of YEAR(CURDATE()))
+-- You should use attributes formed and split for computing the lifespan
+
+
+SELECT band_name , COALESCE(split, 2022) - formed  AS lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC 
