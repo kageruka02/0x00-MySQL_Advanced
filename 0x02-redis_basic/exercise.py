720f07ade05484fe3f36820d58e562b5dee49ed4
kageruka02 <leonkageruka@gmail.com>
2024-06-20 01:11:38 +0300

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,16 +18,21 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
-    
-    def get(self, key:str,  fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, float, None]:
         value = self._redis.get(key)
         if value is None:
             return None
         if fn:
             return fn(value)
         return value
-    def get_str(self, key: str) -> Optional[str]:
-        return self.get(key, lambda d: d.decode('utf-8'))
 
-    def get_int(self, key: str) -> Optional[int]:
-        return self.get(key, lambda d: int(d.decode('utf-8')))
+    def get_str(self, key: str) -> str:
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> str:
+        value = self._redis.get(key)
+        return value.decode("utf-8")
