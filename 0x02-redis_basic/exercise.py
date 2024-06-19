2c7fbf500dc8805f08c0a66e9628be980d26545a
kageruka02 <leonkageruka@gmail.com>
2024-06-20 01:26:56 +0300

    redis basicss

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,18 +9,22 @@ from functools import wraps
     Writing strings to Redis.
 """
 
+
 def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwargs)
+
     return wrapper
+
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
+
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         random_key = str(uuid.uuid4())
